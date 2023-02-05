f6432e5bf8de03e01e045d00af1143f9643fd422
Great-kiola <hollabodey.kilaso@gmail.com>
2023-02-05 13:24:12 +0000

    made changes to the index and js file

---
+++ script.js
@@ -0,0 +1,48 @@
+// Todays Date.
+var today = moment();
+$("#currentDay").text(today.format("[Today's date is]:  MMMM Do YYYY, h:mm:ss a"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const m = moment();
+// // var today = m
+// $("#currentDay").text(m.toString())
+
+
